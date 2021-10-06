bb45b22f6ffd304c61a1bfb0c328d3bb7127ba56
konrad-macialek <konrad.macialek@vonage.com>
2021-10-06 20:55:22 +0200

    MeasurementController added

---
+++ Server/Controllers/MeasurementsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RemoteCarDiagz.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MeasurementsController : ControllerBase
+    {
+        private readonly ILogger<MeasurementsController> _logger;
+
+        public MeasurementsController(ILogger<MeasurementsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Upload([FromBody] byte value)
+        {
+            var locationUri = HttpContext.Request.GetDisplayUrl();
+            return Created(locationUri, Ok());
+        }
+    }
+}
