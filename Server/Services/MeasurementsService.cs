50e106ebc828f1049e10be6a336f4e5e76ab4419
konrad-macialek <konrad.macialek@vonage.com>
2021-10-15 14:18:24 +0200

    New Pid handlers plugged into the pipeline

--- Server/Services/MeasurementsService.cs
+++ Server/Services/MeasurementsService.cs
@@ -10,17 +10,25 @@ namespace RemoteCarDiagz.Server.Services
 
     public class MeasurementService : IMeasurementService
     {
-        private readonly IMeasurementHandler _firstHandler = new EngineRpmHandler();
+        private readonly IMeasurementHandler _engineRpmHandler = new EngineRpmHandler();
 
         public MeasurementService()
         {
-            var coolantTemperatureHandler = new CoolantTemperatureHandler();
-            _firstHandler.SetNext(coolantTemperatureHandler);
+            _engineRpmHandler
+                .SetNext(new CoolantTemperatureHandler())
+                .SetNext(new EngineLoadHandler())
+                .SetNext(new EngineRpmHandler())
+                .SetNext(new FuelPressureHandler())
+                .SetNext(new IntakeAirTemperatureHandler())
+                .SetNext(new IntakeManifoldAbsolutePressureHandler())
+                .SetNext(new MafAirFlowRateHandler())
+                .SetNext(new ThrottlePosition())
+                .SetNext(new VehicleSpeedHandler());
         }
 
         public void ProcessMeasurement(SendMeaurementsRequest request)
         {
-            _firstHandler.Handle(request);
+            _engineRpmHandler.Handle(request);
         }
     }
 }
