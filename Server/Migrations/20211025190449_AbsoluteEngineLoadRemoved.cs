66939801e64a830c11ce980cc71a82ee3f8547d4
konrad-macialek <konrad.macialek@vonage.com>
2021-10-25 21:06:06 +0200

    Migation removing ABSOLUTE_ENGINE_LOAD added

---
+++ Server/Migrations/20211025190449_AbsoluteEngineLoadRemoved.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RemoteCarDiagz.Server.Migrations
+{
+    public partial class AbsoluteEngineLoadRemoved : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Measurements",
+                keyColumn: "Value",
+                keyValue: 67);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Measurements",
+                columns: new[] { "Value", "IsActive", "IsAvailable" },
+                values: new object[] { 67, false, true });
+        }
+    }
+}
