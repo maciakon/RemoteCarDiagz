5cf4b8c678d1914ee9f62aca0f3e3a2fd3563316
maciakon <k.macialek@gmail.com>
2023-07-07 17:24:03 +0200

    InitialConfigurationMqtt client started in hosted service

--- Server/Mqtt/MqttClientHostedService.cs
+++ Server/Mqtt/MqttClientHostedService.cs
@@ -7,15 +7,17 @@ namespace RemoteCarDiagz.Server.Mqtt
     public class MqttClientHostedService : BackgroundService
     {
         private readonly IMeasurementsMqttClient _measurementsMqttClient;
+        private readonly IInitialConfigurationPublisherMqttClient _initialConfigurationPublisherMqttClient;
 
-        public MqttClientHostedService(IMeasurementsMqttClient measurementsMqttClient)
+        public MqttClientHostedService(IMeasurementsMqttClient measurementsMqttClient, IInitialConfigurationPublisherMqttClient initialConfigurationPublisherMqttClient)
         {
             _measurementsMqttClient = measurementsMqttClient;
+            _initialConfigurationPublisherMqttClient = initialConfigurationPublisherMqttClient;
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            return _measurementsMqttClient.Start();
+            return Task.WhenAll(_measurementsMqttClient.Start(), _initialConfigurationPublisherMqttClient.Start());
         }
     }
 }
