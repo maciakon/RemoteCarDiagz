5413e115cb995f3fc84639ca23ec1263deddd7d7
maciakon <k.macialek@gmail.com>
2023-08-20 13:10:55 +0200

    MQTT Client removed. PIdEnum friendly name extensions added.

---
+++ Shared/Domain/PidValueExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace RemoteCarDiagz.Shared.Domain
+{
+    public static class PidValueExtensions
+    {
+        public static string GetFriendlyName(this PidIdsEnum pidId)
+        {
+            return pidId switch
+            {
+                PidIdsEnum.PID_ENGINE_LOAD => "Engine load",
+                PidIdsEnum.PID_COOLANT_TEMP => "Coolant temperature",
+                PidIdsEnum.PID_ENGINE_RPM => "Engine RPM",
+                PidIdsEnum.PID_MAF_FLOW => "Mass air flow sensor air flow rate",
+                PidIdsEnum.PID_INTAKE_MAP => "Intake manifold absolute pressure",
+                PidIdsEnum.PID_FUEL_PRESSURE => "Fuel pressure",
+                PidIdsEnum.PID_THROTTLE => "Throttle position",
+                PidIdsEnum.PID_INTAKE_TEMP => "Intake air temperature",
+                PidIdsEnum.PID_VEHICLE_SPEED => "Vehicle speed",
+                _ => pidId.ToString(),
+            };
+        }
+    }
+}
